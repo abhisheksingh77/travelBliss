7e14e44353c0510196b77b3e10795d0e9592067a
abhisheksingh77 <99204907+abhisheksingh77@users.noreply.github.com>
2022-02-07 23:35:44 +0530

    initial

---
+++ Tourisme.js
@@ -0,0 +1,5 @@
+ var a="<h3>exlore</h3>";
+
+  var target=document.getElementById("framer");
+
+         target.insertAdjacentCSS("before begin",a);
---
+++ relegiouus.js
@@ -0,0 +1,7 @@
+var target= document.getElementByClass("rel");
+
+var a= "<h2>Religious Places</h2>";
+
+
+
+ target.insertAdjacentHTML("afterbegin",a);
